bcc8bd5037c4737f09d10d6a60622fb5896ed45a
Luis Palma <palmaluis1997@gmail.com>
2024-12-18 20:22:58 -0400

    eslint

--- src/components/Company/ChangeCompany.vue
+++ src/components/Company/ChangeCompany.vue
@@ -57,7 +57,7 @@ import { authentication } from 'src/stores/module-authentication'
 import { ref } from 'vue'
 import { useQuasar } from 'quasar'
 import { api } from 'src/boot/axios'
-import { logo } from '../../const/mixins';
+import { logo } from '../../const/mixins'
 
 defineProps({
   companies: {
