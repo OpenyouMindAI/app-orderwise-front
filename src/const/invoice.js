61747badf1b6d49c8b80e1f35a93ee91b352e45c
Luis Palma <palmaluis1997@gmail.com>
2024-12-23 01:15:27 -0400

    tamaño del ticket

--- src/const/invoice.js
+++ src/const/invoice.js
@@ -121,7 +121,7 @@ export const printInvoice = (data, userSession) => {
   const { company_session: companySession } = userSession
   const JsPdf = jsPDF
 
-  const altura = 250
+  const altura = 40 + data.products.length * 5 + 95
 
   const doc = new JsPdf({
     unit: 'mm',
@@ -140,74 +140,74 @@ export const printInvoice = (data, userSession) => {
   }
 
   doc.text(companySession?.name?.toUpperCase(), centrarTexto(companySession?.name?.toUpperCase()), y)
-  y += 7
+  y += 4
   doc.text(companySession?.address?.toUpperCase(), centrarTexto(companySession?.address?.toUpperCase()), y)
-  y += 7
+  y += 4
   doc.text(companySession?.phone_number, centrarTexto(companySession?.phone_number), y)
-  y += 7
+  y += 4
   doc.text(companySession?.document_number?.toUpperCase(), centrarTexto(companySession?.document_number), y)
 
-  y += 7
+  y += 4
   doc.text('----------------------------------------', 5, y)
-  y += 7
+  y += 4
 
   doc.text(`NRO: ${data.code}`, 5, y)
-  y += 7
+  y += 4
   doc.text(`CLIENTE: ${data?.client?.name} ${data?.client?.last_name || ''}`, 5, y)
-  y += 7
+  y += 4
   doc.text(`FECHA: ${formatDate(data.created_at, 'DD/MM/YYYY')}`, 5, y)
-  y += 7
+  y += 4
   doc.text(`HORA: ${formatDate(data.created_at, 'HH:mm:ss')}`, 5, y)
-  y += 7
+  y += 4
   doc.text(`MESERO: ${data?.seller?.name} ${data?.seller?.last_name || ''}`, 5, y)
-  y += 7
+  y += 4
   doc.text(`TIPO: ${data?.invoice_type?.name}`, 5, y)
-  y += 7
+  y += 4
 
   if (data?.tables?.length > 0) {
     data?.tables?.forEach((table) => {
       doc.text(`MESA: ${table?.name} Sala ${table.living_room?.name}`, 5, y)
-      y += 7
+      y += 4
     })
   }
 
   doc.text('----------------------------------------', 5, y)
-  y += 7
+  y += 4
 
   doc.text('Cant./Precio Unit', 5, y)
   doc.text('IMPORTE', 90, y, { align: 'right' })
-  y += 7
+  y += 4
   doc.text('Descripción', 5, y)
-  y += 7
+  y += 4
 
   doc.text('----------------------------------------', 5, y)
 
-  y += 7
+  y += 4
   data.products.forEach((product) => {
     const cantidadPrecio = `${product.pivot.amount} X ${product.pivot.price.toFixed(2)}`
     const subtotal = (product.pivot.amount * product.pivot.price).toFixed(2)
 
     doc.text(cantidadPrecio, 5, y)
     doc.text(subtotal, 90, y, { align: 'right' })
-    y += 7
+    y += 4
 
     const maxWidth = 60
     const description = doc.splitTextToSize(product.name, maxWidth)
     description.forEach((linea) => {
       doc.text(linea, 5, y)
-      y += 7
+      y += 4
     })
   })
 
   doc.text('----------------------------------------', 5, y)
-  y += 7
+  y += 4
 
   doc.text('TOTAL', 5, y)
   doc.text(data.total.toFixed(2), 90, y, { align: 'right' })
-  y += 7
+  y += 4
 
   doc.text('----------------------------------------', 5, y)
-  y += 7
+  y += 4
   doc.text('¡GRACIAS POR SU COMPRA!', centrarTexto('¡GRACIAS POR SU COMPRA!'), y)
 
   return doc
