2b04f03e5ccca3db4fb00f3ea67f86294d2c010c
Luis Palma <palmaluis1997@gmail.com>
2026-01-18 18:41:28 -0400

    transfer

--- src/pages/TransferProductPage.vue
+++ src/pages/TransferProductPage.vue
@@ -750,10 +750,10 @@
                         use-input
                         hide-selected
                         fill-input
-                        option-label="name"
                         option-value="id"
                         input-debounce="500"
                         @filter="getProducts"
+                        :option-label="(opt) => `${opt.name} - ${opt?.unit_of_measure?.name}`"
                         :rules="[val => !!val || 'Requerido']"
                         @update:model-value="(value) => setProducts(value, product)"
                       >
@@ -783,7 +783,6 @@
                       <q-input
                         v-model.number="product.quantity"
                         type="number"
-                        :label="`Cant.${product?.unit_of_measurement?.name}`"
                         outlined
                         dense
                         :step="0.001"
@@ -870,7 +869,7 @@
                         use-input
                         hide-selected
                         fill-input
-                        option-label="name"
+                        :option-label="(opt) => `${opt.name} - ${opt?.unit_of_measure?.name}`"
                         option-value="id"
                         input-debounce="500"
                         @filter="getProducts"
@@ -892,8 +891,7 @@
                         type="number"
                         outlined
                         dense
-                        min="1"
-                        step="1"
+                        :step="0.001"
                         :rules="[val => val > 0 || 'Requerido']"
                         @update:model-value="updateTotals"
                       >
@@ -1125,7 +1123,7 @@
 
           <q-card-actions v-if="!isTransferVerified" align="right" class="q-pa-md">
             <q-btn
-              label="Editar"
+              label="Guardar"
               color="primary"
               type="submit"
               :disable="currentTransfer.products.length === 0 || (editMode && !canEditTransfer)"
