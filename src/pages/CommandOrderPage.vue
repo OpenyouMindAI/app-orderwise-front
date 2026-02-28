4615c6a46e3fccd31b93279548d08bafd3889905
Luis Palma <palmaluis1997@gmail.com>
2026-02-28 13:59:35 -0400

    change

--- src/pages/CommandOrderPage.vue
+++ src/pages/CommandOrderPage.vue
@@ -1526,12 +1526,12 @@ const store = authentication()
 const userSession = store.userSession
 
 const dialogFilter = ref(false)
-const panel = ref('day')
+const panel = ref('between')
 const dateField = ref('delivery_date')
 const dateFilters = ref({
   day: date.formatDate(new Date(), 'YYYY-MM-DD'),
-  from: '',
-  to: '',
+  from: date.formatDate(new Date(), 'YYYY-MM-DD'),
+  to: date.formatDate(date.addToDate(new Date(), { years: 3 }), 'YYYY-MM-DD'),
   fromHours: '00:00',
   toHours: '23:59'
 })
